1734647436cc94a020ff774f450f555578936e72
Chekwasy <richardchekwas@gmail.com>
2023-12-24 19:33:29 +0100

    a

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,7 +9,7 @@ def update_topics(mongo_collection, name, topics):
     """
     update many rows
     """
-    return mongo_collection.update_Many(
+    return mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}},
     )
